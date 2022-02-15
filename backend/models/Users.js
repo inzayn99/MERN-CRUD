be462a5312eabaf3e514f20e60974dde94f95982
inzayn <hungrymindinside99@gmail.com>
2022-02-15 20:25:11 +0545

    Frontend-msg

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -1,14 +1,16 @@
 const mongo = require('mongoose');
-const Scheme =mongoose.Schema;
+const Scheme = mongoose.Schema;
 
-let userSchema=new Scheme({
-    name:{
-        type:String,
+let userSchema = new Scheme({
+    name: {
+        type: String,
     },
-    address:{
-        type:String
+    address: {
+        type: String
     },
-    phone:{
-      type:String
+    phone: {
+        type: String
     }
 });
+
+module.exports = mongoose.model('Users', UserScheme);
